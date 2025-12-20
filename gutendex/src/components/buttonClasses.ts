3b235819fa0eb2bcd963fa7debc2098aadc708e5
Borgerod <borgerod@hotmail.com>
2025-12-20 11:34:51 +0100

    style: format button class definitions for consistency

--- gutendex/src/components/buttonClasses.ts
+++ gutendex/src/components/buttonClasses.ts
@@ -1,41 +1,41 @@
 import { cn } from "@/utils/cn";
 
 export const iconBtnClass = cn(
-  "p-3",
-  "aspect-square",
-  "h-12",
-  "pointer-events-auto",
-  "rounded-full",
-  "self-center",
-  "hover:bg-foreground/10",
-  "",
-  ""
+	"p-3",
+	"aspect-square",
+	"h-12",
+	"pointer-events-auto",
+	"rounded-full",
+	"self-center",
+	"hover:bg-foreground/10",
+	"",
+	""
 );
 
 export const compressedBtnClass = cn("px-5!", "py-1.5!", "h-fit!", "", "");
 
 export const textBtnClass = cn(
-  "aspect-auto",
-  "text-nowrap",
-  "flex items-center justify-center",
-  "p-3",
-  "h-12",
-  "pointer-events-auto",
-  "rounded-full",
-  "",
-  ""
+	"aspect-auto",
+	"text-nowrap",
+	"flex items-center justify-center",
+	"p-3",
+	"h-12",
+	"pointer-events-auto",
+	"rounded-full",
+	"",
+	""
 );
 
 export const primaryBtnClass = cn(
-  "border border-accent-dark text-accent-dark",
-  "hover:bg-accent-dark hover:text-container-solid",
-  "",
-  ""
+	"border border-accent-dark text-accent-dark",
+	"hover:bg-accent-dark hover:text-container-solid",
+	"",
+	""
 );
 
 export const secondaryBtnClass = cn(
-  "border border-secondary text-secondary ",
-  "hover:border-accent-dark hover:text-accent-dark",
-  "",
-  ""
+	"border border-secondary text-secondary ",
+	"hover:border-accent-dark hover:text-accent-dark",
+	"",
+	""
 );
